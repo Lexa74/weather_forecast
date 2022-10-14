c41d814754ee4a996b9373ff9a16c715a89c87bb
Алексей <tany--96@mail.ru>
2022-10-14 15:21:46 +0500

    Оптимизировал код сервиса погоды на главной

--- weatherapp/src/weatherService.js
+++ weatherapp/src/weatherService.js
@@ -2,71 +2,88 @@ import { useEffect } from "react";
 
 const enumAllowedCityes = ['Minsk', 'Moscow', 'Bratislava']
 
-export const getWeather = async (latitude, longitude, cityWeather = 'Minsk') => {
-    //const weatherBlock = document.querySelector('.data-weather')
-    const weatherCityBlock = document.querySelector('.weatherCity')
-    const weatherNowBlock = document.querySelector('.weatherNow')
-    const weatherTomorrowBlock = document.querySelector('.weatherTomorrow')
-    const weatherAfterTomorrowBlock = document.querySelector('.weatherAfterTomorrow')
-    const weatherAfter2DayBlock = document.querySelector('.weatherAfter2Day')
-
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`)
-    const getWeather = await response.json()
-    //Погода завтра в полдень 12 + 24 ед
-    const timeTomorrowWeather = 36;
-    //Погода Послезавтра в полдень 36 + 24 ед
-    const timeAfterTomorrowWeather = 50;
-    const timeAfter2dayWeather = 74;
-    const contentWeatherByBlock = [
-        {
-            weatherCity: cityWeather,
-            weatherNow: getWeather.current_weather.temperature,
-            weatherTomorrow: getWeather.hourly.temperature_2m[timeTomorrowWeather],
-            weatherAfterTomorrow: getWeather.hourly.temperature_2m[timeAfterTomorrowWeather],
-            weatherAfter2Day: getWeather.hourly.temperature_2m[timeAfter2dayWeather]
-        }
-    ]
-    weatherCityBlock.innerHTML = contentWeatherByBlock[0].weatherCity
-    weatherNowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherNow)}°C`
-    weatherTomorrowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherTomorrow)}°C`
-    weatherAfterTomorrowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherAfterTomorrow)}°C`
-    weatherAfter2DayBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherAfter2Day)}°C`
-
-
-    // weatherBlock.innerHTML = ` ${contentWeatherByBlock[0].weatherCity} <br>
-    // Погода сейчас: ${contentWeatherByBlock[0].weatherNow}°C<br>
-    // Погода завтра: ${contentWeatherByBlock[0].weatherTomorrow}°C<br>
-    // Погода послезавтра: ${contentWeatherByBlock[0].weatherAfterTomorrow}°C<br>
-    // Погода через 2 дня: ${contentWeatherByBlock[0].weatherAfter2Day}°C
-    // `
-    console.log(contentWeatherByBlock)
-    return contentWeatherByBlock
+export const weatherDataOutput = async (paramCoord) => {
+    const dataWeather = await getWeather(paramCoord)
+    console.log(await dataWeather[0].weatherCity)
+    // return await getWeather()
 }
 
-//Получить координаты по названию города и установить погоду
-export const setWeatherByCity = async (nameCity) => {
+export const getWeather = async (paramCoord) => {
+    //const weatherBlock = document.querySelector('.data-weather')
+
+    // const weatherCityBlock = document.querySelector('.weatherCity')
+    // const weatherNowBlock = document.querySelector('.weatherNow')
+    // const weatherTomorrowBlock = document.querySelector('.weatherTomorrow')
+    // const weatherAfterTomorrowBlock = document.querySelector('.weatherAfterTomorrow')
+    // const weatherAfter2DayBlock = document.querySelector('.weatherAfter2Day')
+
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?${paramCoord}&hourly=temperature_2m&current_weather=true`)
+    const getDataWeather = await response.json()
+
+    return [
+        {
+            weatherCity: getNameByCity(paramCoord),
+            weatherNow: getDataWeather.current_weather.temperature,
+            weatherTomorrow: getDataWeather.hourly.temperature_2m[36],
+            weatherAfterTomorrow: getDataWeather.hourly.temperature_2m[50],
+            weatherAfter2Day: getDataWeather.hourly.temperature_2m[74]
+        }
+    ]
+
+    //Погода завтра в полдень 12 + 24 ед
+    // const timeTomorrowWeather = 36;
+    // //Погода Послезавтра в полдень 36 + 24 ед
+    // const timeAfterTomorrowWeather = 50;
+    // const timeAfter2dayWeather = 74;
+    // const contentWeatherByBlock = [
+    //     {
+    //         weatherCity: cityWeather,
+    //         weatherNow: getWeather.current_weather.temperature,
+    //         weatherTomorrow: getWeather.hourly.temperature_2m[timeTomorrowWeather],
+    //         weatherAfterTomorrow: getWeather.hourly.temperature_2m[timeAfterTomorrowWeather],
+    //         weatherAfter2Day: getWeather.hourly.temperature_2m[timeAfter2dayWeather]
+    //     }
+    // ]
+    // weatherCityBlock.innerHTML = contentWeatherByBlock[0].weatherCity
+    // weatherNowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherNow)}°C`
+    // weatherTomorrowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherTomorrow)}°C`
+    // weatherAfterTomorrowBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherAfterTomorrow)}°C`
+    // weatherAfter2DayBlock.innerHTML = `${Math.ceil(contentWeatherByBlock[0].weatherAfter2Day)}°C`
+
+
+    // // weatherBlock.innerHTML = ` ${contentWeatherByBlock[0].weatherCity} <br>
+    // // Погода сейчас: ${contentWeatherByBlock[0].weatherNow}°C<br>
+    // // Погода завтра: ${contentWeatherByBlock[0].weatherTomorrow}°C<br>
+    // // Погода послезавтра: ${contentWeatherByBlock[0].weatherAfterTomorrow}°C<br>
+    // // Погода через 2 дня: ${contentWeatherByBlock[0].weatherAfter2Day}°C
+    // // `
+    // return contentWeatherByBlock
+}
+
+//Получить координаты по названию города
+export const getCoordsByCity = async (nameCity) => {
     // const weatherBlock = document.querySelector('.data-weather')
     // weatherBlock.innerHTML = "Loading..."
     const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${nameCity}`)
     const dataNameByCoords = await response.json()
     //Координаты города 
     //await getNameByCity(`latitude=${dataNameByCoords.results[0].latitude}&longitude${dataNameByCoords.results[0].longitude}`)
-    await getWeather(dataNameByCoords.results[0].latitude, dataNameByCoords.results[0].longitude, nameCity)
+    return [`latitude=${await dataNameByCoords.results[0].latitude}&longitude=${await dataNameByCoords.results[0].longitude}`]
+    //await getWeather(dataNameByCoords.results[0].latitude, dataNameByCoords.results[0].longitude, nameCity)
 }
 
 //Получить название города по координатам
 export const getNameByCity = async (paramCoord) => {
     const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?${paramCoord}&localityLanguage=en`)
     const dataCity = await response.json();
-    return dataCity.city;
+    return await dataCity.city
 }
 
 export const resultWeatherInCity = () => {
     const getCity = document.querySelectorAll('.getCity')
     getCity.forEach((city) => {
-        city.addEventListener('click', () => {
-            setWeatherByCity(city.value)
-            //return city.value
+        city.addEventListener('click', async () => {
+            await weatherDataOutput(await getCoordsByCity(city.value))
         })
     })
 
@@ -78,17 +95,20 @@ export const useWeather = () => {
         if (!enumAllowedCityes.includes(cityByUrl)) {
             //Определил геолокацию
             navigator.geolocation.getCurrentPosition(async (position) => {
-                const thisCity = await getNameByCity(`latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`)
-                getWeather(position.coords.latitude, position.coords.longitude, thisCity)
-            }, () => {
-                setWeatherByCity('Minsk')
+                await weatherDataOutput(`latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`)
+            }, async () => {
+                await weatherDataOutput(await getCoordsByCity('Minsk'))
             })
             
         } else {
-            setWeatherByCity(cityByUrl)
+            const awaitForCity = async () => {
+               return weatherDataOutput(await getCoordsByCity(cityByUrl))
+            }
+            awaitForCity();
         }
         resultWeatherInCity();
     }, [])
+    
 }
 
 
